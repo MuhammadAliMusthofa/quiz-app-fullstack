4e2f7f64a653e03d8af456d31b665a5379f49603
Muhammad ALi Musthofa <muhammadalimusthofa786@gmail.com>
2024-12-24 09:23:57 +0700

    add db server 41

--- backend/config/database.js
+++ backend/config/database.js
@@ -21,8 +21,8 @@ let mysql = require('mysql2');
  
 let connection = mysql.createConnection({
    host:        'localhost',
-   user:        'root',
-   password:    '',
+   user:        'developer',
+   password:    'ai.dev@erl',
    database:    'quiz-erklika'
  });
 
