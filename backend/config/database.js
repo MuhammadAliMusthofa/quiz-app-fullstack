6e7b615a7667108e0bf378e7f33e1af55b2a5bdb
Muhammad ALi Musthofa <muhammadalimusthofa786@gmail.com>
2024-12-24 09:12:22 +0700

    add ci

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,29 @@
+// let mysql = require('mysql2');
+ 
+// let connection = mysql.createConnection({
+//    host:        'localhost',
+//    user:        'root',
+//    password:    '',
+//    database:    'kahoot-custom'
+//  });
+
+// connection.connect(function(error){
+//    if(!!error){
+//      console.log(error);
+//    }else{
+//      console.log('Connection Succuessfully!');
+//    }
+//  })
+
+// module.exports = connection; 
+
 let mysql = require('mysql2');
  
 let connection = mysql.createConnection({
    host:        'localhost',
    user:        'root',
    password:    '',
-   database:    'kahoot-custom'
+   database:    'quiz-erklika'
  });
 
 connection.connect(function(error){
